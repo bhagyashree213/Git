73149a92c9694a6816f8da5fe3ed8ebb1268c5de
bhagyashree213 <bhusarebhagyashree6@gmail.com>
2025-09-13 10:36:47 +0530

    New file added

---
+++ Assignment.java
@@ -0,0 +1,20 @@
+class Assignment
+{
+    public static void main(String[] args)
+    {
+        int a = 10;
+        System.out.println("a => " + a); // 10
+
+        a += 5; // a = a + 5
+        System.out.println("a => " + a); // 15
+
+        a *= 2; // a = a * 2
+        System.out.println("a => " + a); // 30
+
+        a /= 6; // a = a / 6
+        System.out.println("a => " + a); // 5
+
+        a -= 2; // a = a - 2
+        System.out.println("a => " + a); // 3
+    }
+}
